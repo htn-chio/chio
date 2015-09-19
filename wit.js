38bd53b390898b60734f02521d5476b0230cebde
katrinacrisostomo <katcri@gmail.com>
2015-09-19 06:20:58 -0400

    added uber to wit.js

--- wit.js
+++ wit.js
@@ -7,6 +7,7 @@ var ACCESS_TOKEN = '73NJLBMT4DZ6E7Q4OEGALQASWS7V2Y7C'
 var FUNC_BY_INTENT = {
     Reminder: parseReminderResponse,
     restaurantSearch: parseYelpResponse,
+    ride: parseUberResponse
 }
 
 module.exports = {
@@ -51,6 +52,17 @@ function parseYelpResponse(res){
         data: {
             locations: _.map(res.entities.location, getValueFromEntity),
             search_queries: _.map(res.entities.search_query, getValueFromEntity),
+        }
+    }
+}
+
+function parseUberResponse(res){
+    return {
+        api: 'Uber',
+        data: {
+            start_location: _.map(res.entities.start, getValueFromEntity),
+            end_location: _.map(res.entities.end, getValueFromEntity),
+            
         }
     }
 }
