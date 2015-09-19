08b4a82f2052472305de3ae3d9b7b45ff3ed2b24
Christopher Luc <chris.luc.dev@gmail.com>
2015-09-19 14:12:55 -0400

    Added Scheduling logic (Sending the message needs to be messaged)

---
+++ models/reminder.model.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var reminderSchema = {
+    user_id: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: [
+            'scheduled',
+            'finished'
+        ]
+    },
+    reminder_date: Date,
+    location: String,
+    task: String
+};
+
+var Reminder = mongoose.model('Reminder', reminderSchema);
+
+module.exports = Reminder;
