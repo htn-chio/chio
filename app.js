65131133abc0a51de3148b34c693096935a62260
Bryan Cho <bcho@redwoodstrategic.com>
2015-09-19 05:32:03 -0400

    almost done uber

--- app.js
+++ app.js
@@ -3,9 +3,20 @@
 /*
  * Express Dependencies
  */
+var request = require('request');
 var express = require('express');
 var app = express();
 var port = 3000;
+var OAuth = require('oauth'), OAuth2 = OAuth.OAuth2;
+
+var clientID = 'SsubnbKVlV6TePcEVhHkEHwUQZUl8tGQ';
+var clientSecret = 'Hp1UDHxwkzlF3opfI-1HnKpaBupAD7J7ShRB6W_m';
+var oauth2 = new OAuth2(clientID,
+                        clientSecret,
+                        '',
+                        'https://login.uber.com/oauth/authorize',
+                        'https://login.uber.com/oauth/token',
+                        null); /** Custom headers */
 
 // For gzip compression
 app.use(express.compress());
@@ -37,6 +48,31 @@ app.set('view engine', 'handlebars');
 // Index Page
 app.get('/', function(request, response) {
     response.render('index');
+});
+
+app.get('/redirect', function(req, res) {
+    console.log(req.query.code);
+    var option = {
+        client_id: clientID,
+        client_secret: clientSecret,
+        redirect_uri: 'localhost:3000/success',
+        grant_type: 'authorization_code'
+    };
+    console.log(option);
+    if(req.query.code) {
+        oauth2.getOAuthAccessToken(req.query.code, option, function (err, access_token, refresh_token) {
+            if (err) {
+                console.log(err);
+            }
+            console.log(access_token);
+        })
+    }
+});
+
+app.get('/authUber', function(req, res) {
+  console.log(oauth2.getAuthorizeUrl({
+    'response_type': 'code'
+  }));
 });
 
 /*
