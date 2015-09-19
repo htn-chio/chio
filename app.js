d959c02c64fdfd57d88e84a037a1453fe8af1bb0
Aaron Te <aaronjte@gmail.com>
2015-09-19 01:56:27 -0400

    Removing useless UI from our generator.

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-/*
- * Use Handlebars for templating
- */
-var exphbs = require('express3-handlebars');
-var hbs;
-
 // For gzip compression
 app.use(express.compress());
 
@@ -20,30 +14,16 @@ app.use(express.compress());
  * Config for Production and Development
  */
 if (process.env.NODE_ENV === 'production') {
-    // Set the default layout and locate layouts and partials
-    app.engine('handlebars', exphbs({
-        defaultLayout: 'main',
-        layoutsDir: 'dist/views/layouts/',
-        partialsDir: 'dist/views/partials/'
-    }));
-
     // Locate the views
     app.set('views', __dirname + '/dist/views');
-    
+
     // Locate the assets
     app.use(express.static(__dirname + '/dist/assets'));
 
 } else {
-    app.engine('handlebars', exphbs({
-        // Default Layout and locate layouts and partials
-        defaultLayout: 'main',
-        layoutsDir: 'views/layouts/',
-        partialsDir: 'views/partials/'
-    }));
-
     // Locate the views
     app.set('views', __dirname + '/views');
-    
+
     // Locate the assets
     app.use(express.static(__dirname + '/assets'));
 }
@@ -51,16 +31,13 @@ if (process.env.NODE_ENV === 'production') {
 // Set Handlebars
 app.set('view engine', 'handlebars');
 
-
-
 /*
  * Routes
  */
 // Index Page
-app.get('/', function(request, response, next) {
+app.get('/', function(request, response) {
     response.render('index');
 });
-
 
 /*
  * Start it up
