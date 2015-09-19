ad4f490e80e81dfdf01bf340b6c5f0a4581e90e5
Christopher Luc <chris.luc.dev@gmail.com>
2015-09-19 15:33:16 -0400

    geocode to location method

---
+++ geocode.js
@@ -0,0 +1,22 @@
+var _ = require('lodash');
+var request = require('request');
+
+var ACCESS_TOKEN = 'spPSmOTYsFpGU74bs5mCNDDu9q6drDNd'
+var LOCATION_PARAM = '&location='
+
+var URL = 'http://www.mapquestapi.com/geocoding/v1/address?key=' + ACCESS_TOKEN + LOCATION_PARAM
+
+
+module.exports = {
+    locationToGeocode: locationToGeocode
+}
+
+function locationToGeocode(location, callback){
+    url = URL + location;
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            callback(null, _.first(_.first(JSON.parse(response.body).results).locations).latLng);
+        }
+        callback(error, null);
+    });
+}
