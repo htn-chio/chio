3dfc8cced243bdff82ec6454969c7cd4068ad0ab
Christopher Luc <chris.luc.dev@gmail.com>
2015-09-19 23:23:31 -0400

    Added State model

---
+++ models/state.model.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var stateSchema = {
+    conversation_id: {
+        type: String,
+        required: true
+    },
+    create_date: Date,
+    meta_data: {},
+    is_active: {
+        type: Boolean,
+        default: true
+    },
+    outcome: {}
+};
+
+var State = mongoose.model('State', stateSchema);
+
+module.exports = State;
